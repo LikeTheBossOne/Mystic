8944162e4db18500e3faabef35ca5df432296e45
Brendan Boss <blboss@ncsu.edu>
2022-04-16 12:06:48 -0500

    updating premakes

--- Dependencies.lua
+++ Dependencies.lua
@@ -3,7 +3,7 @@
 IncludeDir = {}
 IncludeDir["stb_image"] = "%{wks.location}/Mystic/vendor/stb_image"
 IncludeDir["yaml_cpp"] = "%{wks.location}/Mystic/vendor/yaml-cpp/include"
-IncludeDir["GLFW"] = "%{wks.location}/Mystic/vendor/glfw/include"
+IncludeDir["GLFW"] = "%{wks.location}/Mystic/vendor/GLFW/include"
 IncludeDir["Glad"] = "%{wks.location}/Mystic/vendor/Glad/include"
 IncludeDir["ImGui"] = "%{wks.location}/Mystic/vendor/imgui"
 IncludeDir["glm"] = "%{wks.location}/Mystic/vendor/glm"
